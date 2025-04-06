d95bf298481485f7b818a3d382c289e53b56b467
Ayan Nath <ayanjustwon@gmail.com>
2025-04-06 21:37:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server:{
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    allowedHosts: ['googlehersite.onrender.com'],
     proxy:{
       '/api/users/':{
         target:'https://hackathon-backend-hm3i.onrender.com',
