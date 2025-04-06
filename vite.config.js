ed5f385d039b14ae0323e4436342974f2fa06541
Ayan Nath <ayanjustwon@gmail.com>
2025-04-06 21:34:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    port: 3000,
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
     proxy:{
       '/api/users/':{
         target:'https://hackathon-backend-hm3i.onrender.com',
